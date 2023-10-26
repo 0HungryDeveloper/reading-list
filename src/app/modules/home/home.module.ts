b06aad7d30ce6e78dc54b815e0deb34f62430f3f
0HungryDeveloper <hungrystudentdev@gmail.com>
2023-10-26 10:23:39 -0600

    FEAT / MV Added product card component

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -9,9 +9,11 @@ import { StoryComponent } from './components/story/story.component';
 import { LargeCardProductComponent } from './components/large-card-product/large-card-product.component';
 import { SmallCardProductComponent } from './components/small-card-product/small-card-product.component';
 import { NewExperienceComponent } from './components/new-experience/new-experience.component';
+import { ProductsComponent } from './components/products/products.component';
+import { CardProductComponent } from './components/card-product/card-product.component';
 
 @NgModule({
-  declarations: [HomeComponent, BookCardComponent, HeroComponent, StoryComponent, LargeCardProductComponent, SmallCardProductComponent, NewExperienceComponent],
+  declarations: [HomeComponent, BookCardComponent, HeroComponent, StoryComponent, LargeCardProductComponent, SmallCardProductComponent, NewExperienceComponent, ProductsComponent, CardProductComponent],
   imports: [CommonModule, HomeRoutingModule]
 })
 export class HomeModule {}
